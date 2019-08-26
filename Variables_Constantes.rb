b56a87f7eaf691031095ef73a88022fe10ab3f4e
Rome Glez <rome_gs@hotmail.com>
2019-08-26 01:10:57 -0500

    Variables y Constantes

---
+++ Variables_Constantes.rb
@@ -0,0 +1,14 @@
+#Variables
+name = "Rome" #Las variables en minisculas
+
+name = "Ruben" #Cambia el contenido 
+
+puts name #Ruben
+
+#Constantes
+NOMBRE = "Donato" #Las constantes en mayusculas
+
+NOMBRE = "Paco" #No cambia el contenido 
+
+puts NOMBRE #Donato
+
